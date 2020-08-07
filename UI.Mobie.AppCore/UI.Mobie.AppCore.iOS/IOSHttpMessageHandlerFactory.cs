db45c4381930fe46bfca00842637a50045831cf3
huanghaian <huanghaiang@gmail.com>
2020-08-07 15:10:03 +0800

    提交

---
+++ UI.Mobie.AppCore/UI.Mobie.AppCore.iOS/IOSHttpMessageHandlerFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+using Foundation;
+using Microsoft.AspNetCore.Http.Connections.Client;
+using UI.Mobie.AppCore.Abstractions;
+using UIKit;
+
+namespace UI.Mobie.AppCore.iOS
+{
+    public class IOSHttpMessageHandlerFactory : IHttpMessageHandlerFactory
+    {
+        public HttpMessageHandler Handle(HttpMessageHandler messageHandler, HttpConnectionOptions options)
+        {
+            var handler = new NSUrlSessionHandler();
+            handler.Credentials = ((HttpClientHandler)messageHandler).Credentials;
+            handler.DisableCaching = true;
+            return handler;
+        }
+    }
+}
---
+++ UI.Mobie.AppCore/UI.Mobie.AppCore/Abstractions/IHttpMessageHandlerFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http.Connections.Client;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace UI.Mobie.AppCore.Abstractions
+{
+    public interface IHttpMessageHandlerFactory
+    {
+        HttpMessageHandler Handle(HttpMessageHandler messageHandler, HttpConnectionOptions options);
+    }
+}
